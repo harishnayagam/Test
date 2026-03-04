3a87285f93b95c533c3257e2df4c1ecb304c31f7
HARISH NAYAGAM R <harishnayagam18@gmail.com>
2026-03-04 20:47:31 +0530

    added webhook success text

--- test.java
+++ test.java
@@ -3,4 +3,5 @@ public static void main(String[] args) {
   int b=20;
   int sum=a+b;
   System.out.println("The sum of a and b is: " + sum);
+  System.out.println("GitHub WebHook done successfully");
 }
